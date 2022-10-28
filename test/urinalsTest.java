8010f6e6f1902d6b6d38a87da319ba23198bc3f2
Sepideh <sepidehfallah2@gmail.com>
2022-10-27 23:30:28 -0700

    string to integer test

--- test/urinalsTest.java
+++ test/urinalsTest.java
@@ -26,6 +26,24 @@ public class urinalsTest {
     }
 
     @Test
+    public void testStringToIntegerList() {
+       List<Integer> list =  src.stringToIntegerList("1001");
+        Assertions.assertEquals(4, list.size());
+
+        int item0 = list.get(0);
+        Assertions.assertEquals(1, item0);
+
+        int item1 = list.get(1);
+        Assertions.assertEquals(0, item1);
+
+        int item2 = list.get(2);
+        Assertions.assertEquals(0, item2);
+
+        int item3 = list.get(3);
+        Assertions.assertEquals(1, item3);
+    }
+
+    @Test
     public void testCountUrinals() {
         List<Integer> list = new ArrayList<>();
         list.add(1);
@@ -45,4 +63,6 @@ public class urinalsTest {
         String out2 = src.countUrinals(list2);
         Assertions.assertEquals("3", out2);
     }
+
+
 }
